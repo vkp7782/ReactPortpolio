db305c7f5bcfb81db17085a796d7a3053ca0af0d
vishal kumar pandey <160586323+vkp7782@users.noreply.github.com>
2025-10-23 21:33:45 +0530

    Update ThemeToggle.jsx

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -3,16 +3,25 @@ import { Moon, Sun } from "lucide-react";
 import { cn } from "@/lib/utils";
 
 export const ThemeToggle = () => {
-    const [isDarkMode, setisDarkMode] = useState(false);
+    const [isDarkMode, setIsDarkMode] = useState(false);
 
     useEffect(() => {
         const storedTheme = localStorage.getItem("theme");
-        if (storedTheme === 'dark') {
-            setisDarkMode(true);
+
+        // If user has no saved preference → default to dark
+        if (!storedTheme) {
             document.documentElement.classList.add("dark");
-        } else {
-            localStorage.setItem("theme", "light");
-            setisDarkMode(false);
+            localStorage.setItem("theme", "dark");
+            setIsDarkMode(true);
+        } 
+        // Else use saved theme
+        else if (storedTheme === "dark") {
+            document.documentElement.classList.add("dark");
+            setIsDarkMode(true);
+        } 
+        else {
+            document.documentElement.classList.remove("dark");
+            setIsDarkMode(false);
         }
     }, []);
 
@@ -20,11 +29,11 @@ export const ThemeToggle = () => {
         if (isDarkMode) {
             document.documentElement.classList.remove("dark");
             localStorage.setItem("theme", "light");
-            setisDarkMode(false);
+            setIsDarkMode(false);
         } else {
             document.documentElement.classList.add("dark");
             localStorage.setItem("theme", "dark");
-            setisDarkMode(true);
+            setIsDarkMode(true);
         }
     };
 
@@ -32,8 +41,7 @@ export const ThemeToggle = () => {
         <button
             onClick={toggleTheme}
             className={cn(
-                "fixed max-sm:hidden top-5 right-5 z-50 p-2 rounded-full transition-colors duration-300",
-                "focus:outlin-hidden"
+                "fixed max-sm:hidden top-5 right-5 z-50 p-2 rounded-full transition-colors duration-300 focus:outline-none"
             )}
         >
             {isDarkMode ? (
